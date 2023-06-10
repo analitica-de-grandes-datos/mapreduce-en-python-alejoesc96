f37d82382291a941a1d9a91b6dc4ab579621c753
Emmanuel Alejandro Escobar Guzman <126002637+alejoesc96@users.noreply.github.com>
2023-06-10 10:16:34 -0500

    Update reducer.py

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,22 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    atrib = {}
+    for row in sys.stdin:
+        columns = row.split(",")
+        if len(columns) == 2:
+            letter = columns[0]
+            amount = float(columns[1])
+            if letter in atrib:
+                atrib[letter] = {
+                    'max': max(atrib[letter]['max'], amount),
+                    'min': min(atrib[letter]['min'], amount)
+                }
+            else:
+                atrib[letter] = {'max': amount, 'min': amount}
+
+    atrib_sort = sorted(atrib.items(), key=lambda x: x[0])
+    for atrib, value in atrib_sort:
+        sys.stdout.write(f"{atrib}\t{value['max']}\t{value['min']}\n")
