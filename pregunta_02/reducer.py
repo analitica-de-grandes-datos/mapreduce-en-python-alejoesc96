2214d341acd8b3b09f8de6a898d669772156fa49
Emmanuel Alejandro Escobar Guzman <126002637+alejoesc96@users.noreply.github.com>
2023-06-10 10:13:18 -0500

    Update reducer.py

--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+dictionary = {}
+
+for row in sys.stdin:
+    row_separada=row.split(",")
+    if   row_separada[0] in dictionary.keys():
+        if dictionary[row_separada[0]] < int(row_separada[1]):
+            dictionary[row_separada[0]] = int(row_separada[1])
+    else:
+        dictionary[row_separada[0]] = int(row_separada[1])
+
+for key in dictionary.keys():
+  key_edit=key.strip()
+  string_valor=str(dictionary[key])
+  print(key_edit  + "\t" + string_valor)
