6a758b40e868f43b053ca53ea1f5d9b06dd0f4c0
Emmanuel Alejandro Escobar Guzman <126002637+alejoesc96@users.noreply.github.com>
2023-06-10 10:14:48 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,15 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+dict_count = {}
+
+for letter in sys.stdin:
+  if letter in dict_count.keys():
+    dict_count[letter] += 1
+  else:
+    dict_count[letter] = 1
+
+for key in dict_count.keys(): 
+  print(key.replace("\n","") + ","+str(dict_count[key]))
