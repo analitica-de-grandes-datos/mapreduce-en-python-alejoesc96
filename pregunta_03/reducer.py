6a39ca55e215b02b835d5a4b394a4cdafe7d1023
Emmanuel Alejandro Escobar Guzman <126002637+alejoesc96@users.noreply.github.com>
2023-06-10 10:14:04 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+list = []
+
+for row in sys.stdin:
+  row_separada = row.split(",")
+  list.append((row_separada[0],row_separada[1]))
+  list.sort(key=lambda x: x[1])
+
+for tuple in list:
+  sys.stdout.write(tuple[0] + "," + str(tuple[1])) 
