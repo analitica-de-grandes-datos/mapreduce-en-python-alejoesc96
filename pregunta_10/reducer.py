a8b8dfe3e1d1242ce6ebb8f96622b87366da3967
Emmanuel Alejandro Escobar Guzman <126002637+alejoesc96@users.noreply.github.com>
2023-06-10 10:19:19 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+dictionary={}
+for row in sys.stdin:
+    row_separada=row.split(";")
+    numero=int(row_separada[0])
+    row_separada[1]=row_separada[1].strip()
+    claves=row_separada[1].split(",")
+    for clave in claves:
+       clave=str(clave.replace(" ",""))
+       if clave in dictionary.keys():
+          dictionary[clave].append(numero)
+          dictionary[clave].sort()
+       else:
+          dictionary[clave]=[numero]
+
+dictionary_ordenado = {}
+llaves_ordenadas = sorted(dictionary)
+for key in llaves_ordenadas:
+  dictionary_ordenado[key] = dictionary[key]
+
+for clave in dictionary_ordenado.keys():
+    sys.stdout.write(clave + "\t" + str(dictionary_ordenado[clave])[1:-1].replace(" ","")+"\n")
