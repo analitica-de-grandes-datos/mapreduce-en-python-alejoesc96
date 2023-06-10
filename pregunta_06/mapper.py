e9ab2ea6c79e713e2b6497f0131b92fafa08a357
Emmanuel Alejandro Escobar Guzman <126002637+alejoesc96@users.noreply.github.com>
2023-06-10 10:15:57 -0500

    Update mapper.py

--- pregunta_06/mapper.py
+++ pregunta_06/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for row in sys.stdin:
+        columnas = row.split("   ")
+        letra = columnas[0]
+        valor = columnas[2]
+        sys.stdout.write(f"{letra},{valor}\n")
