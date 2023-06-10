f9a024d9f73a3dd9c662429236fa26971b1d85db
Emmanuel Alejandro Escobar Guzman <126002637+alejoesc96@users.noreply.github.com>
2023-06-10 10:12:56 -0500

    Update mapper.py

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+
+for row in sys.stdin:
+  row_separada=row.split(",")
+  sys.stdout.write(row_separada[3]+","+row_separada[4]+ "\n")
