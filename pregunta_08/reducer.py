bc86e65d1a198a5835b944b80c8b640107a25546
Emmanuel Alejandro Escobar Guzman <126002637+alejoesc96@users.noreply.github.com>
2023-06-10 10:17:59 -0500

    Update reducer.py

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,21 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+dict_count = {}
+dict_sum={}
+
+for row in sys.stdin:
+  row_separada=row.split(",")
+  if row_separada[0] in dict_count.keys():
+    dict_count[row_separada[0]] += 1
+    dict_sum[row_separada[0]]+=int(row_separada[1])
+  else:
+    dict_count[row_separada[0]] = 1
+    dict_sum[row_separada[0]]=int(row_separada[1])
+
+for key in dict_count.keys():
+  suma=str(float(dict_sum[key]))
+  promedio=str(dict_sum[key]/dict_count[key])
+  sys.stdout.write(key+"\t"+suma+"\t"+promedio+"\n")
